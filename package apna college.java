d84b6021a0e9aa15053c2735d4619409d3874bd9
Laxmiraman Dnyaneshwar Gudewar <132219054+laxmiramangudewar@users.noreply.github.com>
2025-06-10 23:17:27 +0200

    Add files via upload

---
+++ package apna college.java
@@ -0,0 +1,12 @@
+package com.apnacollege;
+
+public class Main {
+  public static void main(string[] args){
+
+    System.out.println("hello world with java");
+  }
+
+
+
+  
+} 
---
+++ tony.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args, char[] sum){
+
+  
+    int a =25;
+    int b =10;
+    System.out.println(sum);
+
+  }
+  
+}
